8dc149f4521b9c0389d44e36a2187dd63d9d4b2d
Anton Kostiuk <antkos90@gmail.com>
2025-05-07 21:27:32 +0200

    fixed

--- src/server.js
+++ src/server.js
@@ -24,6 +24,7 @@ export const startServer = () => {
       const contacts = await getAllContacts();
 
       res.status(200).json({
+        status: 200,
         message: 'Successfully found contacts!',
         data: contacts,
       });
@@ -38,11 +39,13 @@ export const startServer = () => {
       const contact = await getContactById(contactId);
       if (!contact) {
         res.status(404).json({
+          status: 404,
           message: 'Contact not found',
         });
         return;
       }
       res.status(200).json({
+        status: 200,
         message: `Successfully found contact with id ${contactId}!`,
         data: contact,
       });
@@ -53,6 +56,7 @@ export const startServer = () => {
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
+      status: 404,
       message: 'Not found',
     });
   });
